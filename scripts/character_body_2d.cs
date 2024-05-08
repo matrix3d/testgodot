d566efd1250e0b120832355c09a4979e16ff86e1
lizhi <405628079@qq.com>
2024-05-08 17:02:42 +0800

    跳跃，待机动画，翻转

--- scripts/character_body_2d.cs
+++ scripts/character_body_2d.cs
@@ -5,7 +5,9 @@ public partial class character_body_2d : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-
+	public override void _Ready(){
+		GD.Print("This is Godot.");
+	}
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -33,7 +35,24 @@ public partial class character_body_2d : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
-
+		AnimatedSprite2D myAnimatedSprite2D = GetNode<AnimatedSprite2D>("myAnimatedSprite2D");
+			
+		if (direction.X!=0){
+			myAnimatedSprite2D.FlipH=direction.X<0;
+			
+			
+			GD.Print( myAnimatedSprite2D+""+(direction.X>0?1:-1));
+		}
+		if (IsOnFloor()){
+			if (direction.X == 0)
+				myAnimatedSprite2D.Play("idle");
+			else
+				myAnimatedSprite2D.Play("run");
+		}
+		else{
+			myAnimatedSprite2D.Play("jump");
+		}
+		
 		Velocity = velocity;
 		MoveAndSlide();
 	}
